e640a3557c3b0764e5e0451af5efa6b0c9464a82
Derek Kweon <kdh4646@hanmail.net>
2023-10-05 19:54:49 -0700

    2023-10-05 making custom hooks

---
+++ src/hooks-store/store.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+//global variables
+let globalState = {};
+let listeners = [];
+let actions = {};
+
+//custom hook
+export const useStore = () => {
+  const setState = useState(globalState)[1];
+
+  const dispatch = (actionIdentifier, payload) => {
+    const newState = actions[actionIdentifier](globalState, payload);
+    globalState = { ...globalState, ...newState };
+
+    for (const listener of listeners) {
+      listener(globalState);
+    }
+  };
+
+  useEffect(() => {
+    listeners.push(setState);
+
+    return () => {
+      listeners = listeners.filter((li) => li !== setState);
+    };
+  }, []);
+
+  return [globalState, dispatch];
+};
+
+export const initStore = (userActions, initialState) => {
+  if (initialState) {
+    globalState = { ...globalState, ...initialState };
+  }
+  actions = { ...actions, ...userActions };
+};
